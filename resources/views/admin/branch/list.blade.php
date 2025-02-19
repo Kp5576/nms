6687539b9c21b8c4a176bca07d36e7b298bdccf8
Kuldeep <kp9017472041@gmail.com>
2025-02-19 16:21:56 +0530

    code

--- resources/views/admin/branch/list.blade.php
+++ resources/views/admin/branch/list.blade.php
@@ -40,19 +40,20 @@
                     &nbsp;
 
 
-                    <div class="btn-group dropup">
-                        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
-                            Import
-                        </button>
-                        <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <span class="sr-only">Toggle Dropdown</span>
-                        </button>
-                        <div class="dropdown-menu">
-                          <!-- Dropdown menu links -->
-                          <a href="{{ url('public/build/assets/sample_file_product.xlsx') }}" style="width:100%"
-                     class="btn btn-secondary btn-full">Download Sample File</a>
-                        </div>
-                      </div>
+
+                      <!-- Split button -->
+<div class="btn-group">
+    <button type="button" class="btn btn-success">Action</button>
+    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <span class="caret"></span>
+      <span class="sr-only">Toggle Dropdown</span>
+    </button>
+    <ul class="dropdown-menu">
+      <li><a href="{{ url('public/build/assets/sample_file_product.xlsx') }}" style="width:100%"
+        class="btn btn-secondary btn-full">Download Sample File</a></li>
+
+    </ul>
+  </div>
 
 
                             <div class="table-responsive mt-2">

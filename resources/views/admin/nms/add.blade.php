183c4968c9b2aaa3f269d4b8210acc90800d5407
Kuldeep <kp9017472041@gmail.com>
2025-02-19 17:51:36 +0530

    code

--- resources/views/admin/nms/add.blade.php
+++ resources/views/admin/nms/add.blade.php
@@ -2,7 +2,7 @@
 @section('styles')
 @endsection
 @section('content')
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" /> 
 <style>
    .d-flex {
     display: flex;

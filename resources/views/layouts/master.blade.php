59af721daa497faba1c87051ae8a55003b188e15
Kuldeep <kp9017472041@gmail.com>
2025-02-19 16:38:14 +0530

    code

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -90,7 +90,8 @@
         @include('sweetalert::alert', ['cdn' => 'https://cdn.jsdelivr.net/npm/sweetalert2@9'])
       <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
       <!--bootstrp script-->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
       <script>
          $(function() {
          var url = window.location;

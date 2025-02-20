2ee1e11324f24ff430e90573759442b8970b456f
Kuldeep <kp9017472041@gmail.com>
2025-02-20 18:26:48 +0530

    code

--- resources/views/admin/nms/add.blade.php
+++ resources/views/admin/nms/add.blade.php
@@ -49,8 +49,8 @@
                 <div class="col-md-6">
                    <select name="branch" required class="form-control">
                       <option value="">Select Option</option>
-                      @foreach($data as $dat)
-                          <option  value="{{$dat->branch_name}}">{{$dat->branch_name}} </option>
+                      @foreach($data as $value)
+                          <option  value="{{$value->branch_name}}">{{$value->branch_name}} </option>
                       @endforeach
                    </select>
                 </div>
